4d9a39ae458f9e9f066161fa6d6710baf3d00966
jj <bluesky@example.com>
2025-09-03 16:59:06 +0900

    for react project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; //App.js 파일을 의미
 import reportWebVitals from './reportWebVitals';
 
+// 리액트 라우터를 사용하기위해 BrowserRouter를 App외부에 감싸주는 역할
+// App.js 파일을 index.js 파일을 포함하고 있으므로, BrowserRouters는 index.js 파일에 한번만 명시해주면 됨
+import { BrowserRouter } from 'react-router-dom'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+/* StrictMode 모드는 개발 도중 발생할 수 있는 문제를 꼼꼼히 감지하기 위하여
+rendering을 두번 실행. 따라서, <React.StrictMode> 를 삭제 */
+
 root.render(
-  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,44 @@
+// 'react-router-dom'은 라우팅을 위한 라이브러리
+// 기본적으로 설치가 안되어있음, 차후 설치 해야함
+// npm istall react-router-dom 엔터
+import{Routes,Route} from 'react-router-dom';
+
+// ch02 장
+// 기호 .은 현재 폴더, ..은 상위 폴더 , / 폴더 구분자
+//import 앱이름 from '전체 경로 / 파일명'
+import AppLetConst from './../components/ch02.es6_syntax/let_const';
+import AppTemplateString from './../components/ch02.es6_syntax/template_string';
+import AppSubjectList from './../components/ch02.es6_syntax/make_subject_list';
+import AppMakeFunction from '../components/ch02.es6_syntax/make_function';
+import AppArrowfunction from '../components/ch02.es6_syntax/arrow_function';
+import AppArraymap from '../components/ch02.es6_syntax/array_map';
+import AppSpreadOperator from '../components/ch02.es6_syntax/spread_operator';
+
+
+//ch03장
+import AppClassComponent from '../components/ch03.component/Components01';
+import AppFunctionComponent from '../components/ch03.component/Components02';
+import AppComponentSeparate from '../components/ch03.component/component_separate';
+
+function AppRoutes(){
+    return(
+    <Routes>
+        {/*element 속성에는 컴포넌트 자체가 아니라 jsx 요소를 넣어야합니다. */}
+        <Route path='/let_const' element={<AppLetConst />}/>
+        <Route path='/template_string' element={<AppTemplateString />}/>
+        <Route path='/make_subject_list' element={<AppSubjectList />}/>
+        <Route path='/make_function' element={<AppMakeFunction />}/>
+        <Route path='/arrow_function' element={<AppArrowfunction />}/>
+        <Route path='/array_map' element={<AppArraymap />}/>
+        <Route path='/spread_operator' element={<AppSpreadOperator />}/>
+
+        <Route path='/Components01' element={<AppClassComponent />}/>
+        <Route path='/Components02' element={<AppFunctionComponent />}/>
+        <Route path='/component_separate' element={<AppComponentSeparate />}/>
+    </Routes>
+
+    );
+
+};
+
+export default AppRoutes;
